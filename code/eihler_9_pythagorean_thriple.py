f3cae74dd13f847b7143c801729ae728ac83f385
Mike Kostenko <vopros.m@gmail.com>
2020-05-06 18:28:05 +0300

    Eihler's project. Task 9: pythagorean triple

--- code/eihler_9_pythagorean_thriple.py
+++ code/eihler_9_pythagorean_thriple.py
@@ -1,4 +1,20 @@
 limit = 1000
-a = 0
-b = 1
-c = 999
+a = 2
+b = 3
+c = 995
+
+while c > a + b:
+    if c * c == a * a + b * b:
+        break
+    c -= 1
+    b += 1
+    m = a
+    n = b
+    while m < n:
+        m += 1
+        n -= 1
+        if c * c == m * m + n * n:
+            a = m
+            b = n
+            break
+print(f'{a} + {b} + {c} = 1000')
