acc7519b585b34d7767fb843d12b7da0f6544f6e
Mike Kostenko <vopros.m@gmail.com>
2020-05-24 15:25:38 +0300

    Swap two nibbles

--- pycode/sum_two_in_array.py
+++ pycode/sum_two_in_array.py
@@ -1,7 +1,13 @@
-# Given an array of integers, and an integer ‘K’, find the count of pairs of elements in the array whose sum is equal to 'K'.
+# Given an array of integers, and an integer ‘K’, find the count of pairs
+# of elements in the array whose sum is equal to 'K'.
 #
 # Input:
-# First line of the input contains an integer T, denoting the number of test cases. Then T test cases follow. Each test case consists of two lines. First line of each test case contains 2 space separated integers N and K denoting the size of array and the sum respectively. Second line of each test case contains N space separated integers denoting the elements of the array.
+# First line of the input contains an integer T, denoting the number
+# of test cases. Then T test cases follow. Each test case consists of
+# two lines. First line of each test case contains 2 space separated
+# integers N and K denoting the size of array and the sum respectively.
+# Second line of each test case contains N space separated integers
+# denoting the elements of the array.
 #
 # Output:
 # Print the count of pairs of elements in the array whose sum is equal to the K.
@@ -22,3 +28,16 @@
 # Output
 # 2
 # 6
+
+
+number = int(input('Input number of cases: '))
+while number > 0:
+    number -= 1
+    count = 0
+    size_of_array, summa = list(map(int, input('Input size of array and sum: ').split()))
+    array = list(map(int, input('Input elements of array: ').split()))
+    for i, elem in enumerate(array):
+        for j in range(i + 1, len(array)):
+            if elem + array[j] == summa:
+                count += 1
+    print(f'Number of pairs is {count}')
