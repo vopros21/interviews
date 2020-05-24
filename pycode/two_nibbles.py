0f065e8a9568101008db869586cd3d83fc233326
Mike Kostenko <vopros.m@gmail.com>
2020-05-24 13:21:31 +0300

    new task from an interview

---
+++ pycode/two_nibbles.py
@@ -0,0 +1,21 @@
+# Given a byte, swap the two nibbles in it. For example 100 is be represented as 01100100 in a byte (or 8 bits). The two nibbles are (0110) and (0100). If we swap the two nibbles, we get 01000110 which is 70 in decimal.
+#
+# Input:
+# The first line contains 'T' denoting the number of testcases. Each testcase contains a single positive integer X.
+#
+# Output:
+# In each separate line print the result after swapping the nibbles.
+#
+# Constraints:
+# 1 ≤ T ≤ 70
+# 1 ≤ X ≤ 255
+#
+# Example:
+# Input:
+# 2
+# 100
+# 129
+#
+# Output:
+# 70
+# 24
