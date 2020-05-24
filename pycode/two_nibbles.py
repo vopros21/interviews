a6fe0dfe3ae9b51a6b1e8d5f6a41fa336f243663
Mike Kostenko <vopros.m@gmail.com>
2020-05-24 13:37:55 +0300

    Swap two nibbles

--- pycode/two_nibbles.py
+++ pycode/two_nibbles.py
@@ -1,4 +1,6 @@
-# Given a byte, swap the two nibbles in it. For example 100 is be represented as 01100100 in a byte (or 8 bits). The two nibbles are (0110) and (0100). If we swap the two nibbles, we get 01000110 which is 70 in decimal.
+# Given a byte, swap the two nibbles in it. For example 100 is be represented
+# as 01100100 in a byte (or 8 bits). The two nibbles are (0110) and (0100).
+# If we swap the two nibbles, we get 01000110 which is 70 in decimal.
 #
 # Input:
 # The first line contains 'T' denoting the number of testcases. Each testcase contains a single positive integer X.
@@ -19,3 +21,14 @@
 # Output:
 # 70
 # 24
+
+number = int(input('Number of tests: '))
+while number > 0:
+    number -= 1
+    a = int(input('Input number to convert: '))
+    bin_a = bin(a)
+    str_bin_a = str(bin_a)[2:]
+    str_bin_a = ('00000000' + str_bin_a)[-8:]
+    str_bin_a = '0b' + str_bin_a[4:] + str_bin_a[:4]
+    bin_a = int(str_bin_a, 2)
+    print(f'Swapped number for {a} is {bin_a}')
