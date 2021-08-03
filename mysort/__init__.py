d4e6f31b49c594551f0a0fb41d5e7a6523fc153c
Mike <vopros.m@gmail.com>
2021-08-03 15:40:57 +0100

    Update main class

--- mysort/__init__.py
+++ mysort/__init__.py
@@ -2,11 +2,12 @@ import random
 
 import mysort.quick_sort_hoare
 import mysort.merge_sort
+import mysort.checking
 
 
 if __name__ == '__main__':
-    new_list = random.sample(range(15), 10) + random.sample(range(10), 5)
-    print(*new_list)
-    # mysort.merge_sort.merge_sort(new_list)
+    new_list = random.sample(range(1000000), 10) + random.sample(range(10), 10)
+    print(new_list)
     mysort.quick_sort_hoare.hoare_sort(new_list)
-    print(*new_list)
+    print(new_list)
+    print(mysort.checking.is_sorted(new_list))
