058164a6ad9c2af69ae4216c8a9c674265796c57
Mike <vopros.m@gmail.com>
2022-04-20 23:19:55 +0100

    Implement the longest common prefix task

---
+++ pycode/longest_common_prefix.py
@@ -0,0 +1,36 @@
+# Write a function to find the longest common prefix string amongst an array of strings.
+# If there is no common prefix, return an empty string "".
+#Example 1:
+
+# Input: strs = ["flower", "flow", "flight"]
+# Output: "fl"
+
+# Example 2:
+# Input: strs = ["dog", "racecar", "car"]
+# Output: ""
+# Explanation: There is no common prefix among the input strings.
+
+#Constraints:
+# 1 <= strs.length <= 200
+# 0 <= strs[i].length <= 200
+# strs[i] consists of only lower - case English letters.
+
+def longestCP(strs):
+    i = 0
+    for i in range(201):
+        if i == len(strs[0]):
+            return strs[0]
+        ch = strs[0][i]
+        for s in strs:
+            if i == len(s):
+                return s
+            elif s[i] != ch:
+                return strs[0][:i]
+
+        i += 1
+    return strs[0][:i+1]
+
+
+if __name__ == "__main__":
+    sset = ["flight", "fligh", "fl0ight"]
+    print(longestCP(sset))
