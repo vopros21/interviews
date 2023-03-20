ce07910fd62c20f1ae7fdbf759ad8326d4afb7a6
Mike <vopros.m@gmail.com>
2023-03-20 12:27:22 +0000

    PlusOne leetcode task in Python3

---
+++ leet_code/plus_one.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/plus-one/
+from typing import List
+
+
+# Runtime: 37ms: beats 44.41%
+# Memory: 13.8MB: beats 94.43%
+
+class Solution:
+    def plus_one(self, digits: List[int]) -> List[int]:
+        k = 0
+        for i in range(1, len(digits) + 1):
+            k = digits[-i] + 1
+            digits[-i] = k % 10
+            if k < 10:
+                return digits
+        if k != 0:
+            digits.insert(0, k // 10)
+        return digits
+
+
+if __name__ == "__main__":
+    set1 = [1, 2, 3]
+    print(Solution().plus_one(digits=set1))
