2dfb562cc9f710e128378d6633d7bdb25608ccd1
Mike <vopros.m@gmail.com>
2023-04-15 17:06:24 +0100

    add_two_numbers.py for leetcode
    template

---
+++ leet_code/add_two_numbers.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/add-two-numbers/
+
+# Runtime: ms: beats %
+# Memory: MB: beats %
+
+from typing import Optional
+
+
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        pass
+
+
+if __name__ == '__main__':
+    pass
