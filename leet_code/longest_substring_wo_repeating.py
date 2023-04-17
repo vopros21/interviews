64a0a52251e3a18619d360a792c28cc7f467195d
Mike <vopros.m@gmail.com>
2023-04-17 22:23:04 +0100

    longest_substring_wo_repeating.py template for leetcode

---
+++ leet_code/longest_substring_wo_repeating.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/longest-substring-without-repeating-characters/
+# Runtime: ms: beats %
+# Memory: MB: beats %
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        pass
+
+if __name__ == '__main__':
+    Solution().lengthOfLongestSubstring(None)
