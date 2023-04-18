6f4b743c05646daf33e34e94314e4f25884af08f
Mike <vopros.m@gmail.com>
2023-04-18 15:25:57 +0100

    merge alternatively for leetcode

---
+++ leet_code/a_1768_merge_string_alternately.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/merge-strings-alternately/
+# Runtime: ms: beats %
+# Memory: MB: beats %
+
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        pass
+
+
+if __name__ == '__main__':
+    w1 = "abc"
+    w2 = "pqr"
+    print(Solution().mergeAlternately(w1, w2))
