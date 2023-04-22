93fd26bff7e73657247aca7f4542371d5fa090a7
Mike <vopros.m@gmail.com>
2023-04-22 11:33:57 +0100

    best day to buy and sell for leetcode
    the first iteration

--- leet_code/a_121_best_day_to_buy_and_sell.py
+++ leet_code/a_121_best_day_to_buy_and_sell.py
@@ -1,12 +1,20 @@
 # https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 # Runtime: ms: beats %
 # Memory: MB: beats %
+from typing import List
+
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        pass
+        min_price = 10000
+        profit = 0
+        for i in range(len(prices)):
+            if prices[i] < min_price:
+                min_price = prices[i]
+            profit = max(prices[i:]) - min_price if profit < max(prices[i:]) - min_price else profit
+        return profit
 
 
 if __name__ == '__main__':
-    ps = [7, 1, 5, 3, 6, 4]
+    ps = [7]
     print(Solution().maxProfit(ps))
