dec3dcad36c04a37e7a738325b95c879278ffe9a
Mike <vopros.m@gmail.com>
2023-04-22 11:25:28 +0100

    best day to buy and sell for leetcode

---
+++ leet_code/a_121_best_day_to_buy_and_sell.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+# Runtime: ms: beats %
+# Memory: MB: beats %
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        pass
+
+
+if __name__ == '__main__':
+    ps = [7, 1, 5, 3, 6, 4]
+    print(Solution().maxProfit(ps))
