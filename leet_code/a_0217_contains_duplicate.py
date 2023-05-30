49fd470a883ed68b7655177b36ab67dd4974af45
Mike <vopros.m@gmail.com>
2023-05-30 22:21:48 +0100

    leetcode contains duplicates
    preparing

---
+++ leet_code/a_0217_contains_duplicate.py
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/contains-duplicate/
+
+# Runtime: ms: beats %
+# Memory: MB: beats %
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        pass
