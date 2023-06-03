93c9cb3d8ac282b0f802ed125c714f3f43a0773f
Mike <vopros.m@gmail.com>
2023-06-03 10:32:31 +0100

    leetcode contains duplicates
    first iteration

--- leet_code/a_0217_contains_duplicate.py
+++ leet_code/a_0217_contains_duplicate.py
@@ -1,8 +1,20 @@
 # https://leetcode.com/problems/contains-duplicate/
+from typing import List
+
 
 # Runtime: ms: beats %
 # Memory: MB: beats %
 
 class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        pass
+    def contains_duplicate(self, nums: List[int]) -> bool:
+        i = 1
+        for n in nums:
+            if n in nums[i:]:
+                return True
+            i += 1
+        return False
+
+
+if __name__ == '__main__':
+    nums = [1,2,3,1]
+    print(Solution().contains_duplicate(nums))
