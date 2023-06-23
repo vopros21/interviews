ba026ff20170de37d99e3a6ba6ee2455bbbfec31
Mike <vopros.m@gmail.com>
2023-06-23 22:42:40 +0100

    leetcode: excel sheet column title
    experimenting

--- leet_code/a_0168_excel_sheet_column_title.py
+++ leet_code/a_0168_excel_sheet_column_title.py
@@ -9,11 +9,11 @@ class Solution:
         result = ""
         while column_number > 0:
             result = alphabet[column_number % 26 - 1] + result
-            column_number = column_number // 26
+            column_number = (column_number - 1) // 26
         return result
 
 
 if __name__ == '__main__':
     # lines = [1, 2, 26, 27, 28, 262, 611]
-    for l in range(701):
+    for l in range(1, 701):
         print(l, Solution().convert_to_title(l))
