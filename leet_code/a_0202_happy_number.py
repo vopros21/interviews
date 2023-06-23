dfa235b831694740962d325302bfb705d4253afa
Mike <vopros.m@gmail.com>
2023-06-23 22:46:48 +0100

    leetcode: happy number
    template

---
+++ leet_code/a_0202_happy_number.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/happy-number/
+
+# Runtime: 1548ms: beats 23.9%
+# Memory: 13.8MB: beats 93.49%
+
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        pass
+
+
+if __name__ == '__main__':
+    number = 19
