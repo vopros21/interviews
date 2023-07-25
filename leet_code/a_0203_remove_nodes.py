95a8871df8f1ffba622142b1a73cfece5edb2e5d
Mike <vopros.m@gmail.com>
2023-07-25 16:43:22 +0100

    leetcode: remove nodes

--- leet_code/a_0203_remove_nodes.py
+++ leet_code/a_0203_remove_nodes.py
@@ -1,13 +1,45 @@
 # https://leetcode.com/problems/remove-linked-list-elements/
+# Runtime: 76ms: beats 89.24%
+# Memory: 20.14MB: beats 18.98%
 
-# Runtime: ms: beats %
-# Memory: MB: beats %
+from typing import Optional
+
 
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
 class Solution:
-    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
-        pass
+    def remove_elements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        tmp = head
+        prev = head
+        while tmp:
+            if tmp == head:
+                if tmp.val == val:
+                    head = head.next
+                    tmp = head
+                    prev = head
+                else:
+                    tmp = head.next
+                continue
+            if tmp.val == val:
+                prev.next = tmp.next
+                tmp = tmp.next
+                continue
+            if tmp:
+                prev = tmp
+                tmp = tmp.next
+        return head
+
+
+if __name__ == '__main__':
+    # list_a = ListNode(1, ListNode(2, ListNode(6, ListNode(3, ListNode(4, ListNode(5, ListNode(6)))))))
+    list_a = ListNode(6, ListNode(6, ListNode(6, ListNode(6))))
+    # list_a = ListNode(1, ListNode(2, ListNode(2, ListNode(1))))
+    list_a = Solution().remove_elements(list_a, 6)
+    while list_a:
+        print(list_a.val)
+        list_a = list_a.next
