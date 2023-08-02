4c083155ae63b0fd42d9f97aecb68b8604894a13
Mike <vopros.m@gmail.com>
2023-08-02 15:00:59 +0100

    leetcode: isomorphic strings

--- leet_code/a_0205_isomorphic_strings.py
+++ leet_code/a_0205_isomorphic_strings.py
@@ -1,13 +1,15 @@
 # https://leetcode.com/problems/isomorphic-strings/
 
-# Runtime: ms: beats %
-# Memory: MB: beats %
+# Runtime: 68ms: beats 16.82%
+# Memory: 16.52MB: 81.29beats %
 
 class Solution:
     def is_isomorphic(self, s: str, t: str) -> bool:
         d = {}
         for i in range(len(s)):
             if s[i] not in d.keys():
+                if t[i] in d.values():
+                    return False
                 d[s[i]] = t[i]
                 continue
             if d[s[i]] != t[i]:
@@ -16,7 +18,7 @@ class Solution:
 
 
 if __name__ == '__main__':
-    s1 = ["egg", "foo", "paper"]
-    s2 = ["add", "bar", "title"]
-    for i in range(3):
+    s1 = ["egg", "foo", "paper", "badc"]
+    s2 = ["add", "bar", "title", "baba"]
+    for i in range(len(s1)):
         print(Solution().is_isomorphic(s1[i], s2[i]))
