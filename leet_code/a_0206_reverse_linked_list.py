13ac260ba4d3022c0c6f0e8be822f4e42465bf96
Mike <vopros.m@gmail.com>
2023-08-19 11:56:35 +0100

    leetcode: reformat file

--- leet_code/a_0206_reverse_linked_list.py
+++ leet_code/a_0206_reverse_linked_list.py
@@ -1,16 +1,9 @@
 # https://leetcode.com/problems/reverse-linked-list/
 
-# Definition for singly-linked list.
-
 # Runtime: 65ms: beats 5.43%
 # Memory: 17.8MB: beats 67.28%
 
 from ListNode import ListNode
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-
 
 class Solution(object):
     def reverse_list(self, head):
@@ -33,10 +26,6 @@ class Solution(object):
 
 if __name__ == "__main__":
     list_a = ListNode(1, ListNode(2, ListNode(4)))
-    # list_a = ListNode(2)
-    # list_b = ListNode(1)
-    # list_a = None
-    # list_b = None
     list_a = Solution.reverse_list(Solution(), list_a)
     while list_a:
         print(list_a.val)
